b7f0820073877980bda6c4abbe2c54cd0c5d786d
blacksyan <ilham.adhy@gmail.com>
2018-12-08 11:58:54 +0700

    Add feature basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  test "invalid signup information" do
+  test "invalid signup data" do
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, params: { user: { name: "", email: "invalid@email", password: "halo", password_confirmation: "1234"}}
@@ -9,12 +9,13 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/signup'
   end
 
-  test "valid signup process" do
+  test "valid signup data" do
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name: "Ilham Adi", email: "valid@email.com", password: "halo1234", password_confirmation: "halo1234" } }
     end
   follow_redirect!
   assert_template 'users/show'
+  assert is_logged_in?
   end
 end

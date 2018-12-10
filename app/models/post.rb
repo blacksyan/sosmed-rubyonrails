2fdc63284fda2eaa4901ede0dece85617e9bb485
blacksyan <ilham.adhy@gmail.com>
2018-12-10 11:28:43 +0700

    Add feature post with image

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,17 @@
 class Post < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
+  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 250 }
+  validate  :picture_size
+
+  private
+
+    # validates size of picture
+    def picture_size
+      if picture.size > 2.megabytes
+        errors.add(:picture, 'should be less than 2 MB')
+      end
+    end
 end
